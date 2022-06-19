eed600f7635692d86e541663be4c6c6ffb4f1e62
hasthi200 <107398577+hasthi200@users.noreply.github.com>
2022-06-19 21:35:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94769623263']
+global.premium = ['94769623263']
+global.ownernomer = '94769623263'
+global.ownername = '🐦sl/king,pathum-md'
+global.botname = 'pvm farst bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'st pvm farst'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
